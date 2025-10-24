7415142c33173b615adc4e563dbb9347bb621436
huraultj <julien.hurault@sumeo.io>
2025-10-24 09:17:16 +0200

    update readme

--- src/boring_semantic_layer/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Boring Semantic Layer core package."""
-
-try:
-    from .semantic_api.mcp import create_mcp_server, MCPSemanticModel
-    __all__ = ["create_mcp_server", "MCPSemanticModel"]
-except ImportError:
-    # MCP dependencies not installed
-    __all__ = []
